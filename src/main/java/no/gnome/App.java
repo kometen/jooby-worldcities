301bf7a4f4a74215ef66073dcf91ff1b8b512f80
Claus Guttesen <kometen@gmail.com>
2016-11-02 10:49:08 +0100

    Changed from params() to param(). Had used first by error.

--- src/main/java/no/gnome/App.java
+++ src/main/java/no/gnome/App.java
@@ -15,6 +15,9 @@ import org.jooby.Mutant;
 import org.jooby.hbm.Hbm;
 import org.jooby.hbm.UnitOfWork;
 import org.jooby.json.Jackson;
+import com.javadocmd.simplelatlng.LatLng;
+import com.javadocmd.simplelatlng.LatLngTool;
+import com.javadocmd.simplelatlng.util.LengthUnit;
 
 /**
  * @author jooby generator
@@ -41,24 +44,69 @@ public class App extends Jooby {
 
         get("/cities", () -> {
             return require(UnitOfWork.class).apply(em -> {
-                return em.createQuery("from City")
+                return em.createQuery("from City c")
                     .setMaxResults(100)
                     .getResultList();
             });
         });
         
         get("/cities/:name", req -> {
-            Map<String, Mutant> name = req.params("name").toMap();
             return require(UnitOfWork.class).apply(em -> {
-                return em.createQuery("from City where name = :name")
-                    .setParameter("name", name.get("name").value())
+                return em.createQuery("from City c where c.name = :name")
+                    .setParameter("name", req.param("name").value())
                     .getResultList();
             });
+        });
+        
+        get("/cities/:latitude/:longitude", req -> {
+            Integer distanceKM = 20;
+            String lat = "latitude";
+            String lon = "longitude";
+            Float latitude;
+            Float longitude;
+            
+            // Try extracting latitude from params.
+            try {
+                latitude = Float.parseFloat(req.param(lat).value());
+            } catch (NumberFormatException e) {
+                latitude = Float.parseFloat("61.7428745");
+            }
+            
+            // Try extracting longitude from params.
+            try {
+                longitude = Float.parseFloat(req.param(lon).value());
+            } catch (NumberFormatException e) {
+                longitude = Float.parseFloat("6.3968833");
+            }
+            
+            // Define center.
+            LatLng center = new LatLng(latitude, longitude);
+
+            // Get latitude west and east of center.
+            LatLng latitude_west = LatLngTool.travel(center, 180, distanceKM, LengthUnit.KILOMETER);
+            LatLng latitude_east = LatLngTool.travel(center, 0, distanceKM, LengthUnit.KILOMETER);
+            
+            // Get longitude south and north of center.
+            LatLng longitude_south = LatLngTool.travel(center, 270, distanceKM, LengthUnit.KILOMETER);
+            LatLng longitude_north = LatLngTool.travel(center, 90, distanceKM, LengthUnit.KILOMETER);
+            
+            /*return "west: " + (float)latitude_west.getLatitude() + ", east: " + (float)latitude_east.getLatitude() +
+                ", south: " + (float)longitude_south.getLongitude() + ", north: " + (float)longitude_north.getLongitude();*/
+            
+            return require(UnitOfWork.class).apply(em -> {
+                return em.createQuery("from City c where c.latitude between :latitude_west and :latitude_east and c.longitude between :longitude_low and :longitude_high")
+                    .setParameter("latitude_west", (float)latitude_west.getLatitude())
+                    .setParameter("latitude_east", (float)latitude_east.getLatitude())
+                    .setParameter("longitude_low", (float)longitude_south.getLongitude())
+                    .setParameter("longitude_high", (float)longitude_north.getLongitude())
+                    .getResultList();
+            });
+            
         });
 
         get("/import", () -> {
             return require(UnitOfWork.class).apply(em -> {
-                List<City> cities = em.createQuery("from City").setMaxResults(1).getResultList();
+                List<City> cities = em.createQuery("from City c").setMaxResults(1).getResultList();
                 if (cities.size() == 1) {
                     return "Data have already been imported!";
                 } else {
