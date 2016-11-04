c48de8d6f63466f114946d9e26c504086886a8b1
Claus Guttesen <kometen@gmail.com>
2016-11-04 10:53:58 +0100

    Added Caffeine cache.

--- src/main/java/no/gnome/App.java
+++ src/main/java/no/gnome/App.java
@@ -1,5 +1,7 @@
 package no.gnome;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,6 +19,7 @@ import com.javadocmd.simplelatlng.LatLng;
 import com.javadocmd.simplelatlng.LatLngTool;
 import com.javadocmd.simplelatlng.util.LengthUnit;
 import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 import org.jooby.Request;
 
 /**
@@ -26,7 +29,19 @@ public class App extends Jooby {
 
     private final static String FILENAME = "worldcitiespop.txt";
 
+    Cache<String, List<City>> cache = Caffeine.newBuilder()
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .maximumSize(10_000)
+            .build();
+
     {
+            
+        use(new Jackson());
+
+        use(new Hbm("jdbc:postgresql://localhost/worldcities")
+                .classes(City.class)
+        );
+
         get("/", (req, rsp) -> {
             String data = "<head><title>gnome.no</title></head>Hello Jooby World!";
             rsp.status(200)
@@ -36,18 +51,21 @@ public class App extends Jooby {
 
         get("/favicon.ico", () -> "");
 
-        use(new Jackson());
+        get("/cities", (req) -> {
+            
+            String key = "cities";
+            List<City> locations = cache.getIfPresent(key);
 
-        use(new Hbm("jdbc:postgresql://localhost/worldcities")
-                .classes(City.class)
-        );
-
-        get("/cities", () -> {
-            return require(UnitOfWork.class).apply(em -> {
-                return em.createQuery("from City c")
-                    .setMaxResults(100)
-                    .getResultList();
-            });
+            if (locations == null) {
+                locations = require(UnitOfWork.class).apply(em -> {
+                    return em.createQuery("from City c")
+                            .setMaxResults(100)
+                            .getResultList();
+                });
+                cache.put(key, locations);
+            }
+            
+            return locations;
         });
         
         get("/cities/:name", req -> {
@@ -128,39 +146,47 @@ public class App extends Jooby {
         } else {
             radius = defaultRadius;
         }
+        
+        // Key used to lookup in Caffeine cache.
+        String key = latitude.toString() + longitude.toString() + radius.toString();
+        List<City> locations = cache.getIfPresent(key);
 
+        if (locations == null) {
             // Define center.
-        LatLng center = new LatLng(latitude, longitude);
+            LatLng center = new LatLng(latitude, longitude);
 
-        // Get latitude west and east of center.
-        LatLng latitude_west = LatLngTool.travel(center, 180, radius, LengthUnit.KILOMETER);
-        LatLng latitude_east = LatLngTool.travel(center, 0, radius, LengthUnit.KILOMETER);
+            // Get latitude west and east of center.
+            LatLng latitude_west = LatLngTool.travel(center, 180, radius, LengthUnit.KILOMETER);
+            LatLng latitude_east = LatLngTool.travel(center, 0, radius, LengthUnit.KILOMETER);
 
-        // Get longitude south and north of center.
-        LatLng longitude_south = LatLngTool.travel(center, 270, radius, LengthUnit.KILOMETER);
-        LatLng longitude_north = LatLngTool.travel(center, 90, radius, LengthUnit.KILOMETER);
+            // Get longitude south and north of center.
+            LatLng longitude_south = LatLngTool.travel(center, 270, radius, LengthUnit.KILOMETER);
+            LatLng longitude_north = LatLngTool.travel(center, 90, radius, LengthUnit.KILOMETER);
 
-        // Get locations within a square.
-        List<City> locations = require(UnitOfWork.class).apply(em -> {
-            return em.createQuery("from City c where c.latitude between :latitude_west and :latitude_east and c.longitude between :longitude_low and :longitude_high")
-                    .setParameter("latitude_west", (float) latitude_west.getLatitude())
-                    .setParameter("latitude_east", (float) latitude_east.getLatitude())
-                    .setParameter("longitude_low", (float) longitude_south.getLongitude())
-                    .setParameter("longitude_high", (float) longitude_north.getLongitude())
-                    .getResultList();
-        });
-        
-        // Define iterator that will traverse the list and remove locations outside the radius.
-        Iterator<City> locationsIterator = locations.iterator();
-        while (locationsIterator.hasNext()) {
-            City city = locationsIterator.next();
-            LatLng endpoint = new LatLng(city.getLatitude(), city.getLongitude());
-            double distance = LatLngTool.distance(center, endpoint, LengthUnit.KILOMETER);
-            if (distance > radius) {
-                locationsIterator.remove();
+            // Get locations within a square.
+            locations = require(UnitOfWork.class).apply(em -> {
+                return em.createQuery("from City c where c.latitude between :latitude_west and :latitude_east and c.longitude between :longitude_low and :longitude_high")
+                        .setParameter("latitude_west", (float) latitude_west.getLatitude())
+                        .setParameter("latitude_east", (float) latitude_east.getLatitude())
+                        .setParameter("longitude_low", (float) longitude_south.getLongitude())
+                        .setParameter("longitude_high", (float) longitude_north.getLongitude())
+                        .getResultList();
+            });
+
+            // Define iterator that will traverse the list and remove locations outside the radius.
+            Iterator<City> locationsIterator = locations.iterator();
+            while (locationsIterator.hasNext()) {
+                City city = locationsIterator.next();
+                LatLng endpoint = new LatLng(city.getLatitude(), city.getLongitude());
+                double distance = LatLngTool.distance(center, endpoint, LengthUnit.KILOMETER);
+                if (distance > radius) {
+                    locationsIterator.remove();
+                }
             }
+
+            cache.put(key, locations);
         }
-        
+
         return locations;
     }
     
